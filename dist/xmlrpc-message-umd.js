76f61c55a80da49be7a628d9af477f4e9553b305
t1st3 <contact@tiste.org>
2014-02-21 21:35:53 +0100

    v0.1.2

--- dist/xmlrpc-message-umd.js
+++ dist/xmlrpc-message-umd.js
@@ -3,7 +3,7 @@
  * 
  * @link https://github.com/T1st3/xmlrpc-message-umd
  * @author T1st3
- * @version 0.1.1
+ * @version 0.1.2
  * @license https://github.com/T1st3/xmlrpc-message-umd/blob/master/LICENSE-MIT
  * @license https://github.com/T1st3/xmlrpc-message-umd/blob/master/LICENSE-BSD
  * @license https://github.com/T1st3/xmlrpc-message-umd/blob/master/LICENSE-GPL
