a01aba5862079d539acb7ae8bbfe667e8c7d76c6
t1st3 <contact@tiste.org>
2014-04-23 15:20:46 +0200

    Fix dependencies problem on btoa-umd

--- docs/assets/js/lib/xmlrpc-message-umd.js
+++ docs/assets/js/lib/xmlrpc-message-umd.js
@@ -3,7 +3,7 @@
 * 
 * @link https://github.com/T1st3/xmlrpc-message-umd
 * @author T1st3
-* @version 0.3.0
+* @version 0.3.1
 * @license https://github.com/T1st3/xmlrpc-message-umd/blob/master/LICENSE
 * 
 * 
