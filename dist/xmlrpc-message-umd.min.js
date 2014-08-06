143fe9a548b681c2f4dde1a1175dfae35eeed46e
t1st3 <contact@tiste.org>
2014-08-06 20:00:28 +0200

    0.7.2

--- dist/xmlrpc-message-umd.min.js
+++ dist/xmlrpc-message-umd.min.js
@@ -3,7 +3,7 @@
 *
 * @link https://github.com/T1st3/xmlrpc-message-umd
 * @author T1st3
-* @version 0.7.0
+* @version 0.7.2
 * @license https://github.com/T1st3/xmlrpc-message-umd/blob/master/LICENSE
 *
 *
